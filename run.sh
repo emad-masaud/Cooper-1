69ece892a2fdb40e1f264a47e055fbe473a3d7c9
emad_masoud <bohneen@gmail.com>
2026-07-31 05:29:57 +0300

    feat: add script to initialize custom doctypes and wrapper for execution in docker environment

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker exec -i frappe_docker-backend-1 bash -c "cd /home/frappe/frappe-bench && env/bin/python -c \"
+import sys
+import frappe
+frappe.init(site='frontend')
+frappe.connect()
+
+code = open('/tmp/create_doctypes.py').read()
+exec(code)
+\""
