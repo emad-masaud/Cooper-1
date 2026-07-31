b362535481bdec9a330b74e5725af206f9bd1ce1
emad_masoud <bohneen@gmail.com>
2026-07-31 05:40:03 +0300

    chore: update site name from frontend to meamart.local in run script

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 docker exec -i frappe_docker-backend-1 bash -c "cd /home/frappe/frappe-bench && env/bin/python -c \"
 import sys
 import frappe
-frappe.init(site='frontend')
+frappe.init(site='meamart.local')
 frappe.connect()
 
 code = open('/tmp/create_doctypes.py').read()
