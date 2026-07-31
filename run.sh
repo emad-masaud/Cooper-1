6e4cefbd2f4d8e735ef549ccf08d408ad43cb7dc
emad_masoud <bohneen@gmail.com>
2026-07-31 05:48:38 +0300

    refactor: update run.sh to generate site API keys directly via bench execute command

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-cd /root/frappe_docker
-docker cp /tmp/get_keys.py frappe_docker-backend-1:/home/frappe/frappe-bench/get_keys.py
-docker compose exec backend env/bin/python get_keys.py
+docker compose exec backend bench --site frontend execute "
+user = frappe.get_doc('User', 'Administrator')
+user.generate_keys()
+user.save()
+frappe.db.commit()
+print('API_KEY=' + user.api_key)
+print('API_SECRET=' + user.api_secret)
+"
